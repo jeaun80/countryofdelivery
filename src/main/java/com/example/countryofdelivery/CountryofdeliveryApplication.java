5c68c6fafd19a7e5637aaa9932ab0285fee84ea6
jeaun80 <kims1706@naver.com>
2022-01-03 18:13:41 +0900

    Initial commit

---
+++ src/main/java/com/example/countryofdelivery/CountryofdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.countryofdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryofdeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryofdeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/countryofdelivery/CountryofdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countryofdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryofdeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
